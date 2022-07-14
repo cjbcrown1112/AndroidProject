e4e84a86efa5e766e9b9e3f96778e59dd3587de4
reginaliza <reginaliza.diloy@gcash.com>
2022-07-15 02:05:28 +0800

    working instagram api

---
+++ RecipeApp/app/src/main/java/ph/gcash/cadet/kotlin/project/recipeapp/entities/Category.kt
@@ -0,0 +1,19 @@
+package ph.gcash.cadet.kotlin.project.recipeapp.entities
+
+import androidx.room.*
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import ph.gcash.cadet.kotlin.project.recipeapp.entities.converter.CategoryListConverter
+
+@Entity(tableName = "Category")
+data class Category (
+
+    @PrimaryKey(autoGenerate = true)
+    var id:Int,
+
+    @ColumnInfo(name = "categoryItems")
+    @Expose
+    @SerializedName("categories")
+    @TypeConverters(CategoryListConverter::class)
+    var categoryitems: List<CategoryItems>? = null
+)
